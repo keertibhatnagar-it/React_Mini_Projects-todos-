3fc793198cb37c353c73659dc84613c37b7490df
Keerti Bhatnagar <71969398+keertibhatnagar-it@users.noreply.github.com>
2021-09-27 10:55:57 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an About component
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse provident odio laboriosam debitis quod, repellat vitae, alias temporibus distinctio labore, obcaecati consectetur voluptatibus tenetur corporis accusamus. Suscipit vero officia cumque.
+                Quidem neque ex tempora aperiam perspiciatis, doloremque molestiae sapiente reiciendis praesentium atque officia, quasi asperiores quas inventore eveniet et. Veniam exercitationem enim non fugiat odio praesentium nam dolores porro ex.
+                Soluta non excepturi laboriosam asperiores explicabo illo inventore, corporis est, nam aspernatur quis aliquid deleniti aliquam aut eveniet magnam incidunt consequatur? Expedita, autem enim cupiditate minus consequatur nobis corrupti tenetur!
+                Est iure enim animi voluptatibus eligendi incidunt aliquid optio accusamus perferendis? Consequuntur eveniet modi ab quae temporibus molestias libero! Temporibus accusamus, molestiae doloribus laborum velit sapiente ea nostrum atque laboriosam.</p>
+                
+        </div>
+    )
+}
